0ee2ab4bb5e899f98a6fea88bbae230f2827a39e
YMG WORKS <59523977+YMG-CODE@users.noreply.github.com>
2022-09-14 23:30:57 +0900

    Add files via upload

--- ete/config.h
+++ ete/config.h
@@ -67,8 +67,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define BACKLIGHT_BREATHING
 
 #define RGB_DI_PIN D3
-//#ifdef RGB_DI_PIN
-#    define RGBLED_NUM 1
+#ifdef RGBLIGHT_ENABLE
+#define RGBLED_NUM 2// Number of LEDs
+#define RGBLED_SPLIT \
+        { 1, 1 }
+#define RGBLIGHT_SPLIT
+#endif
+
 #    define RGBLIGHT_HUE_STEP 8
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
@@ -88,11 +93,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 /*== customize breathing effect ==*/
 /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
+//#    define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
 /*==== use exp() and sin() ====*/
-#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-//#endif
+//#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+//#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
